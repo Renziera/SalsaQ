da53353dde21afbd4f436dc3229ace9b9041a3c4
Renziera <stanleyheryanto@gmail.com>
2019-10-31 23:42:20 +0700

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SalsaQ
 Bot Line anti-unsend
+
+Bot ini diciptakan untuk memerangi unsend di Line yang dipelopori oleh Salsa setelah berguru pada master Juan.
+
+#### Add bot
+ID @919qrkzz
+
+Scan QR Code
+
+
+#### Command
+SalsaQ resend [number of messages]
+
+### Upcoming features
+* Repo kode nuklir
+* Show sender display name
+* Support for sticker & image messages
---
+++ functions/index.js
@@ -0,0 +1,116 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const config = require('./credential');
+
+admin.initializeApp();
+
+const app = express();
+const client = new line.Client(config);
+const db = admin.firestore();
+
+app.use(line.middleware(config));
+
+app.post('/', (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+});
+
+async function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    let timestamp = event.timestamp;
+    let id = '';
+
+    switch (event.source.type) {
+        case 'user':
+            id = event.source.userId;
+            break;
+        case 'group':
+            id = event.source.groupId;
+            break;
+        case 'room':
+            id = event.source.roomId;
+            break;
+    }
+
+    let message = event.message.text;
+
+    if (isCommand(message)) {
+        let reply = await processCommand(message, id);
+        let data = { type: 'text', text: reply };
+        return client.replyMessage(event.replyToken, data);
+    }
+
+    await db.collection('chatrooms').doc(id).collection('chats').add({
+        message: message,
+        timestamp: timestamp,
+        waktu: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    return Promise.resolve(null);
+}
+
+function isCommand(text) {
+    text = text.toLowerCase();
+
+    if (text === 'salsaq tsundere mode on') return true;
+
+    if (!text.startsWith('salsaq ')) return false;
+
+    let arr = text.split(' ');
+    if (arr.length > 3) return false;
+    if (arr[1] === 'resend') {
+        if (isNaN(arr[2])) return false;
+    } else {
+        if (isNaN(arr[1])) return false;
+    }
+    return true;
+}
+
+async function processCommand(message, id) {
+
+    if (message.toLowerCase() === 'salsaq tsundere mode on') {
+        return 'It\'s not like I wanted to follow your order or something you baka >_<';
+    }
+
+    let amount = 10;
+    let x = message.split(' ')[message.split(' ').length - 1];
+    if (x > 0) {
+        if (x > 69) {
+            amount = 69;
+        } else {
+            amount = parseInt(x);
+        }
+    }
+
+    let result = await db.collection('chatrooms').doc(id)
+        .collection('chats').orderBy('timestamp', 'desc')
+        .limit(amount).get();
+
+    if (result.empty) {
+        return '>_<';
+    }
+
+    let reply = '';
+
+    for (let i = 0; i < result.docs.length; i++) {
+        let data = result.docs[i].data();
+        reply = reply.concat(data.message);
+        if (i != result.docs.length - 1) {
+            reply = reply.concat('\n\n------------\n\n');
+        }
+    }
+
+    return reply;
+}
+
+exports.bot = functions.region('asia-east2').https.onRequest(app);
